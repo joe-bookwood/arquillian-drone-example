327fce3b76311b3c2f1670f3f888782efb931fc2
Jochen buchholz <jochen.buchholz@ifasec.de>
2019-07-25 19:14:16 +0200

    test webarchive rewritten

--- src/test/java/de/bitc/view/CustomerWebIT.java
+++ src/test/java/de/bitc/view/CustomerWebIT.java
@@ -10,8 +10,6 @@ import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,13 +34,33 @@ public class CustomerWebIT {
 
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "web-test.war")
-                .addClasses(CustomerBean.class, Customer.class)
+        WebArchive warArchive = ShrinkWrap.create(WebArchive.class, "web-test.war")
+                .addClasses(CustomerBean.class, Customer.class, ViewUtils.class)
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-                .addAsWebResource(new File(WEBAPP_SRC_CUSTOMER, "search.xhtml"))
-                .addAsWebResource(new File(WEBAPP_SRC_TEMPLATE, "pageTemplate.xhtml"))
-                .addAsWebInfResource(new StringAsset("<faces-config version=\"2.3\"/>"), "faces-config.xml")
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addAsResource("META-INF/forge.taglib.xml", "META-INF/forge.taglib.xml")
+                .addAsWebResource(new File("src/main/webapp/resources/true.png"),"/resources/true.png")
+                .addAsWebResource(new File("src/main/webapp/resources/remove.png"),"/resources/remove.png")
+                .addAsWebResource(new File("src/main/webapp/resources/forge-style.css"),"/resources/forge-style.css")
+                .addAsWebResource(new File("src/main/webapp/resources/forge-logo.png"),"/resources/forge-logo.png")
+                .addAsWebResource(new File("src/main/webapp/resources/scaffold/pageTemplate.xhtml"),"/resources/scaffold/pageTemplate.xhtml")
+                .addAsWebResource(new File("src/main/webapp/resources/scaffold/paginator.xhtml"),"/resources/scaffold/paginator.xhtml")
+                .addAsWebResource(new File("src/main/webapp/resources/add.png"),"/resources/add.png")
+                .addAsWebResource(new File("src/main/webapp/resources/bootstrap.css"),"/resources/bootstrap.css")
+                .addAsWebResource(new File("src/main/webapp/resources/search.png"),"/resources/search.png")
+                .addAsWebResource(new File("src/main/webapp/resources/false.png"),"/resources/false.png")
+                .addAsWebResource(new File("src/main/webapp/resources/favicon.ico"),"/resources/favicon.ico")
+                .addAsWebResource(new File("src/main/webapp/resources/jboss-community.png"),"/resources/jboss-community.png")
+                .addAsWebResource(new File("src/main/webapp/error.xhtml"))
+                .addAsWebResource(new File("src/main/webapp/customer/view.xhtml"),"/customer/view.xhtml")
+                .addAsWebResource(new File("src/main/webapp/customer/create.xhtml"),"/customer/create.xhtml")
+                .addAsWebResource(new File("src/main/webapp/customer/search.xhtml"),"/customer/search.xhtml")
+                .addAsWebResource(new File("src/main/webapp/index.xhtml"))
+                .addAsWebResource(new File("src/main/webapp/index.html"))
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/faces-config.xml"), "faces-config.xml")
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/web.xml"), "web.xml")
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"), "beans.xml");
+        System.out.println(warArchive.toString(true));
+        return warArchive;
     }
 
     @Test
